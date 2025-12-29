e3115038abdcc0565ff111ce77668cf9ff1c01b5
Vương Minh Hiếu <hieulklk128@gmail.com>
2025-12-29 11:38:02 +0700

    update 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-auth")
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
     implementation("com.google.firebase:firebase-firestore")
 
 
