685ea8b272b16f56bd2868b118aac100d62ced23
Vương Minh Hiếu <hieulklk128@gmail.com>
2025-12-30 14:41:08 +0700

    update 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
 
 
 }
